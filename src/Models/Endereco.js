370b703ea17f785391d20fc372071829eadba615
dieissonmartins <dieisson.martins.santos@gmail.com>
2020-09-04 15:48:30 -0300

    criado model tabela Endereco

---
+++ src/Models/Endereco.js
@@ -0,0 +1,47 @@
+
+//Modulos
+const Sequelize     = require('sequelize');
+const connection    = require('../database/conexao');
+
+//Model
+const Endereco = connection.define('enderecos', {
+    tipo:{
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    cep:{
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    estado:{
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    cidade:{
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    bairro:{
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    logradouro:{
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    numero:{
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    complemento:{
+        type: Sequelize.TEXT,
+        allowNull: false
+    },
+});
+
+
+//atualizar tabela no banco de dados
+Endereco.sync({force: true});
+
+//exportar modulo
+module.exports = Endereco;
