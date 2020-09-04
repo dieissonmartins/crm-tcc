b3c54f3cb2e96fd86101cc034cc40e8e79366a5f
dieissonmartins <dieisson.martins.santos@gmail.com>
2020-09-04 19:09:22 -0300

    criado metodos crud de cliente

---
+++ src/Http/Controllers/clienteController.js
@@ -0,0 +1,29 @@
+
+
+exports.index = (requisicao, resposta) => {
+    resposta.render('admin/clientes/index');
+};
+
+exports.create = (requisicao, resposta) => {
+    resposta.render('admin/clientes/create');
+};
+
+exports.store = (requisicao, resposta) => {
+
+};
+
+exports.show = (requisicao, resposta) => {
+    resposta.render('admin/clientes/show');
+};
+
+exports.edit = (requisicao, resposta) => {
+    resposta.render('admin/clientes/edit');
+};
+
+exports.update = (requisicao, resposta) => {
+    
+};
+
+exports.destroy = (requisicao, resposta) => {
+    
+};
