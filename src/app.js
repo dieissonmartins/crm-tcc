74ddf76fa86d21de2ae2cb6550b9e60fc40d41c6
dieissonmartins <dieisson.martins.santos@gmail.com>
2020-09-04 15:41:14 -0300

    iniciar servidor

--- src/app.js
+++ src/app.js
@@ -1,2 +1,48 @@
 
-console.log("teste");
+//modulos
+const express    = require('express');
+const bodyParser = require('body-parser');
+const connection = require('./database/conexao');
+const session    = require('express-session');
+
+//routes
+const routes = require('./routes/routes');
+
+//models
+const Cliente   = require('./Models/Cliente'); 
+const Contrato  = require('./Models/Contrato');  
+
+//instância do express
+const app = express();
+
+//View engine
+app.set('view engine', 'ejs');
+
+//sessions active
+app.use(session({
+    secret: "qweasdzxcrtyfghvbnuiopjklçnm",
+    cookie:{
+        maxAge: 100000 //deslogar em 10 minutos
+    }
+}));
+
+//EJS arquivos estaticos 
+app.use(express.static('public'));
+
+//Body parser
+app.use(bodyParser.urlencoded({extends: false})); // aceitar dados de formularios 
+app.use(bodyParser.json()); // aceitar dados no formato JSON
+
+//utilizar rotas
+app.use('/', routes);
+
+//Database
+connection.authenticate()
+    .then(() => {
+        console.log('Conexão feita com sucesso!');
+    }).catch((error) => {
+        console.log(error);
+    });
+
+//exportar modulo
+module.exports = app;
---
+++ src/server.js
@@ -0,0 +1,7 @@
+
+const app = require('./app');
+
+//iniciar servidor
+app.listen(8080, () => {
+    console.log(`Servidor rodando na porta...`);
+});
