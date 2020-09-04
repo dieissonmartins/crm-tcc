e34805218df37931f020ffad122f2c2cc28927a7
dieissonmartins <dieisson.martins.santos@gmail.com>
2020-09-04 15:47:17 -0300

    criado model tabela Cliente

---
+++ src/Models/Cliente.js
@@ -0,0 +1,34 @@
+
+//Modulos
+const Sequelize     = require('sequelize');
+const connection    = require('../database/conexao');
+const Pessoa        = require('./Pessoa');
+const Contrato      = require('./Contrato');
+const Empresa       = require('./Empresa');
+
+//Model
+const Cliente = connection.define('clientes', {
+    nome:{
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    telefone:{
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+});
+
+//relacionamento com cliente | MUITAS Pessoas para UM Cliente
+Pessoa.hasMany(Cliente);
+
+//relacionamento com Empresa | MUITAS Empresas para UM Cliente
+Empresa.hasMany(Cliente);
+
+//relacionamento com contratos | MUITOS contratos para UM Cliente
+Contrato.hasMany(Cliente);
+
+//atualizar tabela no banco de dados
+Cliente.sync({force: true});
+
+//exportar modulo
+module.exports = Cliente;
