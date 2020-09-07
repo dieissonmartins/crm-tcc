9554d30124da168f2a12e5832f780ee3f3f57011
dieissonmartins <dieisson.martins.santos@gmail.com>
2020-09-07 20:41:57 -0300

    add CDNs datatables e feito listagem de clientes

---
+++ public/assets/js/datatables.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    //table folha
+    $('#tabela-clientes').DataTable({
+        responsive: true,
+        "pageLength": 10,
+        dom: 'Bfrtip',
+        buttons: [
+            {
+                extend: 'pdfHtml5',
+                pageSize: 'TABLOID'
+            },
+            'excel','csv','print','copy' 
+        ],
+        "language": {
+            "url": "//cdn.datatables.net/plug-ins/1.10.19/i18n/Portuguese-Brasil.json"
+        }
+    });
+
+    //quebra linha do objeto
+    $("#tabela-clientes tbody tr #nome").css("white-space","pre-wrap");
+});
