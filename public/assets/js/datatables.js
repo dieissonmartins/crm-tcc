d321e44dabec960d3fd01436e5e6d4e12b1fa369
dieissonmartins <dieisson.martins.santos@gmail.com>
2020-09-10 19:40:53 -0300

    criado datatables de servicos e produtos

--- public/assets/js/datatables.js
+++ public/assets/js/datatables.js
@@ -37,5 +37,25 @@ $(document).ready(function() {
     });
 
     //quebra linha do objeto
-    $("#tabela-clientes tbody tr #nome").css("white-space","pre-wrap");
+    $("#tabela-servicos tbody tr #nome").css("white-space","pre-wrap");
+
+      //table produtos
+      $('#tabela-produtos').DataTable({
+        responsive: true,
+        "pageLength": 10,
+        dom: 'Bfrtip',
+        buttons: [
+            {
+                extend: 'pdfHtml5',
+                pageSize: 'TABLOID'
+            },
+            'excel','csv','print','copy' 
+        ],
+        "language": {
+            "url": "//cdn.datatables.net/plug-ins/1.10.19/i18n/Portuguese-Brasil.json"
+        }
+    });
+
+    //quebra linha do objeto
+    $("#tabela-produtos tbody tr #nome").css("white-space","pre-wrap");
 });
