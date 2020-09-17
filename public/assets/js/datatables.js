4e859d84ddbeddb9dda634a4586d63487aa01ee5
dieissonmartins <dieisson.martins.santos@gmail.com>
2020-09-17 11:29:15 -0300

    add datatables tabela-pessoas

--- public/assets/js/datatables.js
+++ public/assets/js/datatables.js
@@ -58,4 +58,24 @@ $(document).ready(function() {
 
     //quebra linha do objeto
     $("#tabela-produtos tbody tr #nome").css("white-space","pre-wrap");
+
+    //table produtos
+    $('#tabela-pessoas').DataTable({
+        responsive: true,
+        "pageLength": 5,
+        dom: 'Bfrtip',
+        buttons: [
+            {
+                extend: 'pdfHtml5',
+                pageSize: 'TABLOID'
+            },
+            'excel','csv','print','copy' 
+        ],
+        "language": {
+            "url": "//cdn.datatables.net/plug-ins/1.10.19/i18n/Portuguese-Brasil.json"
+        }
+    });
+
+    //quebra linha do objeto
+    $("#tabela-produtos tbody tr #nome").css("white-space","pre-wrap");
 });
